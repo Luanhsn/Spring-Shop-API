d65de93887dccf085ed7a987e0f0078fbd41867f
Luanhsn <luan.hyseni09@gmail.com>
2026-06-11 16:17:32 +0200

    feat: adding validation test to price and name

--- src/main/java/com/luan/shopapi/product/Product.java
+++ src/main/java/com/luan/shopapi/product/Product.java
@@ -3,6 +3,8 @@ package com.luan.shopapi.product;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 
 @Entity
 public class Product {
@@ -10,7 +12,11 @@ public class Product {
     @Id
     @GeneratedValue
     private Long id;
+
+    @NotBlank
     private String name;
+
+    @Positive
     private double price;
 
     public Product() {}
